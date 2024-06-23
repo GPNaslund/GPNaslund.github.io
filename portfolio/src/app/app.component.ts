201275bcfaed80b70a423759987f71fc0621e021
Gustav Näslund <gpnaslund@gmail.com>
2024-06-23 18:17:10 +0200

    Updates portfolio page

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TerminalComponent } from './components/terminal/terminal.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TerminalComponent],
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
