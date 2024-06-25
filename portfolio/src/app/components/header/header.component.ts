21ffab949fd26bd449641d14b88f4290237048be
Gustav Näslund <gpnaslund@gmail.com>
2024-06-25 07:10:05 +0200

    Adds light dark features

--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OnInit, AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit, AfterViewInit {
+  isDarkMode: boolean = false;
+
+  ngOnInit(): void {
+    this.checkDarkMode();
+  }
+
+  ngAfterViewInit(): void {
+    this.addDarkModeListener();
+  }
+
+  private checkDarkMode() {
+    if (window.matchMedia) {
+      const query = window.matchMedia('(prefers-color-scheme: dark)');
+      this.isDarkMode = query.matches;
+    }
+  }
+
+  private addDarkModeListener(): void {
+    if (window.matchMedia) {
+      const query = window.matchMedia('(prefers-color-scheme: dark)');
+      query.addEventListener('change', (event) => {
+        this.isDarkMode = event.matches;
+      });
+    }
+  }
 
 }
